3b9ef4257467d88decaab29b0d80b8f073e366aa
Helen Murugan <gallonhelen@hotmail.com>
2023-06-27 20:15:00 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
+
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def username():
+    """
+    Request name input and welcome user to the game
+    """
+    username = input("Enter your name here:\n")
+
+If validate_data(username):
+	print(f"Hi {username}, welcome to Scrambled Tech.\n")
+	break
+
+def validate_data(username):
+    """
+    Validate there are a minimum of 2 letters for username entry.
+    If entry is empty or does not contain a minimum of two letters, 
+    raise Error and ask for name entry again.
+    """
+
+
+
+username()
+
+
