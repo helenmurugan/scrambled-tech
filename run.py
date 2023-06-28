3b308011e37f00d91e1d7e250adf9f69f0d10f41
Helen Murugan <gallonhelen@hotmail.com>
2023-06-28 19:48:41 +0000

    feat: Add username function to request user's name. Add validate_data function to check that the name contains a minimum of two characters and does not contain special characters

--- run.py
+++ run.py
@@ -1,25 +1,43 @@
 
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+def validate_data(name):
+    """
+    Validate there are a minimum of 2 letters for the name entry.
+    If entry is empty or does not contain a minimum of two letters, 
+    raise ValueError and ask for name entry again.
+    """
+    invalid_chars = ['!', '@', '#', '$', '%', '.', ':', ';', '?', '/']  # Specify the list of invalid characters
+    for char in invalid_chars:
+        if char in name:
+            raise ValueError("Invalid characters found in name. Please try again.")
 
+
+    if len(name) < 2:
+        raise ValueError("Your name entry must have a minimum of two letters. Please try again")
+    else:
+        return True
+
+    
 def username():
     """
-    Request name input and welcome user to the game
+    Request name input and welcome user to the game.
     """
-    username = input("Enter your name here:\n")
-
-If validate_data(username):
-	print(f"Hi {username}, welcome to Scrambled Tech.\n")
-	break
-
-def validate_data(username):
-    """
-    Validate there are a minimum of 2 letters for username entry.
-    If entry is empty or does not contain a minimum of two letters, 
-    raise Error and ask for name entry again.
-    """
+    while True:
+        try:
+            name = input("Enter your name here:\n")
+            if validate_data(name):
+	            print(f"Hi {name}, welcome to Scrambled Tech.\n")
+	            break
+        except ValueError as e:
+            print("Invalid name:", str(e))
 
 
 
-username()
+
+
+def main():
+    username()
+
+main()
 
 
