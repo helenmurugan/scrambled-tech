40f9dec559e16b5aa5b14197ec53f123d7b29fb5
Helen Murugan <gallonhelen@hotmail.com>
2023-06-28 20:32:23 +0000

    fix: Add letter_count variable to count letters(not numbers). Name entry with only numbers will not pass validation. feat: Add navigation function

--- run.py
+++ run.py
@@ -2,17 +2,19 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 def validate_data(name):
     """
-    Validate there are a minimum of 2 letters for the name entry.
-    If entry is empty or does not contain a minimum of two letters, 
-    raise ValueError and ask for name entry again.
+    Validate there are a minimum of 2 letters in the name entry.
+    Check name entry for invalid special characters.
+    Raise ValueError is name entry is not valid.
     """
-    invalid_chars = ['!', '@', '#', '$', '%', '.', ':', ';', '?', '/', '(', ')', '{', '}', '[', ']', '<', '>']  
+    invalid_chars = ['!', '@', '#', '$', '%', '.', ':', ';', '?', '/', '(', ')', '{', '}', '[', ']', '<', '>', '+', '=']  
     
     if any(char in name for char in invalid_chars):
-        raise ValueError("Invalid characters found in name. Please try again.")
+        raise ValueError("Invalid characters found in name. Please try again.\n")
 
-    if len(name) < 2:
-        raise ValueError("Your name entry must have a minimum of two letters. Please try again.")
+    letter_count = sum(1 for char in name if char.isalpha())
+
+    if letter_count < 2:
+        raise ValueError("Your name entry must have a minimum of two letters. Please try again.\n")
     
     return True
 
@@ -31,11 +33,24 @@ def username():
             print("Invalid name:", str(e))
 
 
+def navigation():
+    """
+    Display navigation options. 
+    The user can choose between three navigation options – play game, how to play or leaderboard
+    """
+    print("Where would you like to go?:\n")
+    print("1. Play Game")
+    print("2. How To Play")
+    print("3. Leaderboard")
+
+
+
 
 
 
 def main():
     username()
+    # navigation()
 
 main()
 
