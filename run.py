3f5345ed9bc7da5c7aef2261c86757e9a8a174c6
Helen Murugan <gallonhelen@hotmail.com>
2023-06-29 20:19:40 +0000

    feat: Add validate_nav function to allow only '1', '2' or '3' as valid navigation options

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ def validate_data(name):
     """
     Validate there are a minimum of 2 letters in the name entry.
     Check name entry for invalid special characters.
-    Raise ValueError is name entry is not valid.
+    Raise ValueError if name entry is not valid.
     """
     invalid_chars = ['!', '@', '#', '$', '%', '.', ':', ';', '?', '/', '(', ')', '{', '}', '[', ']', '<', '>', '+', '=']  
     
@@ -27,30 +27,49 @@ def username():
         try:
             name = input("Enter your name here:\n")
             if validate_data(name):
-	            print(f"Hi {name}, welcome to Scrambled Tech.\n")
-	            break
+                print()
+                print(f"Hi {name}, welcome to Scrambled Tech.\n")
+                break
         except ValueError as e:
             print("Invalid name:", str(e))
+
+
+def validate_nav(nav_choice):
+    """
+    Validate that an entry '1','2' or '3' has been made.
+    Raise ValueError if any other entry has been made.
+    Prompt user to enter another navigation choice.
+    """
+    if nav_choice != "1" and nav_choice != "2" and nav_choice != "3":
+        raise ValueError("Please select where you would like to go (1,2 or 3):\n")
+
+    return True
 
 
 def navigation():
     """
     Display navigation options. 
-    The user can choose between three navigation options – play game, how to play or leaderboard
+    The user can choose between three navigation options (Play Game, How To Play or Leaderboard).
     """
-    print("Where would you like to go?:\n")
-    print("1. Play Game")
-    print("2. How To Play")
-    print("3. Leaderboard")
-
-
-
+    while True:
+        try:
+            print("Where would you like to go (1,2 or 3)?:\n")
+            print("1. Play Game")
+            print("2. How To Play")
+            print("3. Leaderboard")
+            nav_choice = input("\n")
+            if validate_nav(nav_choice):
+                print()
+                print(f"You chose {nav_choice}")
+                break
+        except ValueError as e:
+            print("Invalid entry:", str(e))
 
 
 
 def main():
     username()
-    # navigation()
+    navigation()
 
 main()
 
