ffe656167b284f119fb5643c10f079286bb0ab70
Helen Murugan <gallonhelen@hotmail.com>
2023-07-07 15:03:19 +0000

    Wire up APIs and confirm access to google sheets

--- run.py
+++ run.py
@@ -1,7 +1,24 @@
+import gspread
+from google.oauth2.service_account import Credentials
 import os
 import random
 import time
 import threading
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('scrambled_tech')
+
+leaderboard = SHEET.worksheet('leaderboard')
+data = leaderboard.get_all_values()
+print(data)
 
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
@@ -495,7 +512,7 @@ def main():
     username()
     navigation()
 
-main()
+# main()
 # level_selection()
 
 
